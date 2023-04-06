9523f462d6484d38971859f5db104ff1bbd7200e
afeiship <1290657123@qq.com>
2023-04-06 19:17:27 +0800

    refactor: use new build script

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
+import nx from '@jswork/next';
 
-  nx.get2get = function (inTarget, inPaths, inValue) {
-    var res;
-    var len = inPaths.length;
-    for (var i = 0; i < len; i++) {
-      var path = inPaths[i];
-      res = nx.get(inTarget, path);
-      if (typeof res !== 'undefined') return res;
-      if (i === len - 1) return inValue;
-    }
-    return res;
-  };
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.get2get;
+nx.get2get = function (inTarget, inPaths, inValue) {
+  var res;
+  var len = inPaths.length;
+  for (var i = 0; i < len; i++) {
+    var path = inPaths[i];
+    res = nx.get(inTarget, path);
+    if (typeof res !== 'undefined') return res;
+    if (i === len - 1) return inValue;
   }
-})();
+  return res;
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = nx.get2get;
+}
+
+export default nx.get2get;
