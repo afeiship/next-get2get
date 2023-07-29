c447c4c8e923fdcfe0f03a9e15155cbbce8bd8d5
afeiship <1290657123@qq.com>
2023-07-29 11:21:12 +0800

    fix: option be global

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
-interface Options {
-  isEmpty: (v: any) => boolean;
-}
 interface NxStatic {
-  get2get: (target: any, paths: string[], value?: any, opts?: Options) => any;
+  get2get: (
+    target: any,
+    paths: string[],
+    value?: any,
+    opts?: {
+      isEmpty: (v: any) => boolean;
+    }
+  ) => any;
 }
